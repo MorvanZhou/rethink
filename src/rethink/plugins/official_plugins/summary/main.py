a1a86ba66cc9ed9d74306626af908bbdd4639ce7
morvanzhou <morvanzhou@hotmail.com>
2024-03-21 10:02:15 +0800

    update plugin

--- src/rethink/plugins/official_plugins/summary/main.py
+++ src/rethink/plugins/official_plugins/summary/main.py
@@ -1,3 +1,7 @@
+import json
+from datetime import datetime
+from pathlib import Path
+
 from jinja2 import Template
 
 import rethink
@@ -10,38 +14,56 @@ class DailySummary(rethink.Plugin):
     description = "summary my daily usage"
     author = "morvanzhou"
     template = Template(
-        f"<h1>{name}</h1>"
-        f"<p>today new thoughts: {{{{ node_count }}}}</p>"
-        f"<p>today word count: {{{{ word_count }}}}</p>"
-        f"<p>last day thoughts: {{{{ last_node_count }}}}</p>"
-        f"<p>last day word count: {{{{ last_word_count }}}}</p>"
+        (Path(__file__).parent / "template.html").read_text(encoding="utf-8")
     )
     schedule_timing = rethink.schedule.every_day_at(hour=0, minute=0)
 
     def __init__(self):
         super().__init__()
-        self.today_node_count = 0
-        self.today_word_count = 0
-        self.last_day_node_count = 0
-        self.last_day_word_count = 0
+        self.data_path = Path(__file__).parent / ".data.json"
+        today = datetime.now().strftime("%Y-%m-%d")
+        if not self.data_path.exists():
+            self.data = [{
+                "date": today,
+                "word": 0,
+                "node": 0,
+            }]
+            self.data_path.write_text(json.dumps(self.data))
+        else:
+            self.data = json.loads(self.data_path.read_text())
+            if self.data[0]["date"] != today:
+                self.data.insert(0, {
+                    "date": today,
+                    "word": 0,
+                    "node": 0,
+                })
+                self.data = self.data[:120]
+                self.write_to_file()
 
     def on_node_updated(self, node: rethink.tps.Node, old_md: str) -> None:
-        self.today_word_count += len(node["md"]) - len(old_md)
+        self.data[0]["word"] += len(node["md"]) - len(old_md)
+        self.write_to_file()
 
     def on_node_added(self, node: rethink.tps.Node) -> None:
-        self.today_word_count += len(node["md"])
-        self.today_node_count += 1
+        self.data[0]["word"] += len(node["md"])
+        self.data[0]["node"] += 1
+        self.write_to_file()
 
     def on_schedule(self):
-        self.last_day_word_count = self.today_word_count
-        self.last_day_node_count = self.today_node_count
-        self.today_word_count = 0
-        self.today_node_count = 0
+        self.data.insert(0, {
+            "date": datetime.now().strftime("%Y-%m-%d"),
+            "word": 0,
+            "node": 0,
+        })
+        self.data = self.data[:120]
+        self.write_to_file()
 
     def render(self):
+        d = self.data[:7]
         return self.template.render(
-            node_count=self.today_node_count,
-            word_count=self.today_word_count,
-            last_node_count=self.last_day_node_count,
-            last_word_count=self.last_day_word_count,
+            name=self.name,
+            data=d,
         )
+
+    def write_to_file(self):
+        self.data_path.write_text(json.dumps(self.data, indent=2, ensure_ascii=False))
