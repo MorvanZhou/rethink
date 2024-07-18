3a7974bb2881d877d8e4b09965e96f61b99e098b
morvanzhou <morvanzhou@hotmail.com>
2024-07-18 22:29:36 +0800

    feat(llm): - concurrent request - update llm functions

--- tests/test_core_utils.py
+++ tests/test_core_utils.py
@@ -29,7 +29,7 @@ class UtilsTest(unittest.IsolatedAsyncioTestCase):
         await asyncio.gather(*tasks)
         total_time = time.time() - st
         self.assertGreaterEqual(total_time, 0.3)
-        self.assertLess(total_time, 0.45)
+        self.assertLess(total_time, 0.5)
         self.assertEqual(11, count)
 
     @patch("httpx.AsyncClient.get")
@@ -72,7 +72,7 @@ class UtilsTest(unittest.IsolatedAsyncioTestCase):
         await asyncio.gather(*tasks)
         total_time = time.time() - st
         self.assertGreaterEqual(total_time, 0.3)
-        self.assertLess(total_time, 0.45)
+        self.assertLess(total_time, 0.5)
         self.assertEqual(5, count)
 
     async def test_concurrent_with_rate_limiter(self):
@@ -93,5 +93,5 @@ class UtilsTest(unittest.IsolatedAsyncioTestCase):
         await asyncio.gather(*tasks)
         total_time = time.time() - st
         self.assertGreaterEqual(total_time, 0.3)
-        self.assertLess(total_time, 0.45)
+        self.assertLess(total_time, 0.5)
         self.assertEqual(4, count)
